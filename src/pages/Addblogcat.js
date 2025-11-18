e8cf412f255939729f5095943559072c6b49a9b0
Vinali Samarasinghe <vinalisamarasinghe@gmail.com>
2025-11-18 22:23:13 +0530

    add new pages

---
+++ src/pages/Addblogcat.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import CustomInput from "../components/CustomInput";
+
+export const Addblogcat = () => {
+  return (
+    <div>
+        <h3 className="mb-4 title">Add Blog Category</h3>
+        <div>
+            <form action="">
+                <CustomInput type="text" label="Enter Blog Category" />
+   
+                <button type="submit" className="btn btn-success border-0 rounded-3 my-5">Add Blog Category</button>
+            </form>
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/Addcat.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import CustomInput from "../components/CustomInput";
+
+const Addcat = () => {
+  return (
+    <div>
+        <h3 className="mb-4 title">Add Category</h3>
+        <div>
+            <form action="">
+                <CustomInput type="text" label="Enter Category" />
+   
+                <button type="submit" className="btn btn-success border-0 rounded-3 my-5">Add Category</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Addcat
