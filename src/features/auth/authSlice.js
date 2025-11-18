f76e2c057e352f4767055cca4fbb548930745304
Vinali Samarasinghe <vinalisamarasinghe@gmail.com>
2025-11-18 23:59:11 +0530

    update

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
+const userDefaultState = {
+  _id: null,
+  firstname: null,
+  lastname: null,
+  email: null,
+  mobile: null,
+  token: null,
+};
+
+const initialState = {
+  user: userDefaultState,
+  isLoading: false,
+  isError: false,
+    isSuccess: false,
+    message: '',
+};
+
+export const login = createAsyncThunk('auth/admin-login', async (user, thunkAPI) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data);
+  }
+});
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {   },
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.user = action.payload;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.isSuccess = false;
+      state.user = null;
+    });
+  }
+})
+
+export default authSlice.reducer;
